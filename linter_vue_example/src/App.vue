0898ca43026416e62b22b3a7926b4ade91dbe583
rxliuli <rxliuli@gmail.com>
2019-09-24 17:13:55 +0800

    测试提交

--- linter_vue_example/src/App.vue
+++ linter_vue_example/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -11,18 +11,18 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
 <style>
 #app {
+  margin-top: 60px;
+  color: #2c3e50;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
