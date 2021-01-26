c530239d498740b3aa5a202e22f83bfef3dcb711
rxliuli <rxliuli@gmail.com>
2021-01-26 23:29:07 +0800

    使用 minmax() 属性解决 grid 受到图片大小固有值的影响 参考规范：https://drafts.csswg.org/css-grid/#layout-algorithm

--- grid_image_layout_demo/src/App.tsx
+++ grid_image_layout_demo/src/App.tsx
@@ -1,6 +1,6 @@
-import faker from 'faker'
-import React from 'react'
-import css from './App.module.css'
+import faker from "faker";
+import React from "react";
+import css from "./App.module.css";
 
 /**
  * 切片分析结果 UI 图片列表
@@ -16,35 +16,35 @@ export function AIResultUIItemImageList(props: {
     <div className={css.aiResultUiItemImageList}>
       {props.imgs.map((item) => (
         <div>
-          <img src={item.url} alt={item.url}/>
+          <img src={item.url} alt={item.url} />
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-const mockImageList = Array(faker.random.number({min: 10, max: 20}))
+const mockImageList = Array(faker.random.number({ min: 10, max: 20 }))
   .fill(0)
   .map(() => {
-    const getNumber = () => faker.random.number({min: 200, max: 300})
-    return [getNumber(), getNumber()]
+    const getNumber = () => faker.random.number({ min: 200, max: 300 });
+    return [getNumber(), getNumber()];
   })
   .map(([x, y]) => ({
     width: x,
     height: y,
-    url: `https://picsum.photos/seed/picsum/${x}/${y}`,
-  }))
+    url: `https://picsum.photos/seed/picsum/${x}/${y}`
+  }));
 
 function App() {
   return (
     <div className={css.aiResultUi}>
-      <AIResultUIItemImageList imgs={mockImageList}/>
-      <AIResultUIItemImageList imgs={mockImageList}/>
-      <AIResultUIItemImageList imgs={mockImageList}/>
-      <AIResultUIItemImageList imgs={mockImageList}/>
-      <AIResultUIItemImageList imgs={mockImageList}/>
+      <AIResultUIItemImageList imgs={mockImageList} />
+      <AIResultUIItemImageList imgs={mockImageList} />
+      <AIResultUIItemImageList imgs={mockImageList} />
+      <AIResultUIItemImageList imgs={mockImageList} />
+      <AIResultUIItemImageList imgs={mockImageList} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
